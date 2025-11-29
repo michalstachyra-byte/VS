253630928ef0d2e50ce07795a6884aea4b3f4ed7
Michał Stachyra <michal.stachyra@edu.uekat.pl>
2025-11-29 19:11:20 +0100

    zadanie5lab2

---
+++ zad5.py
@@ -0,0 +1,10 @@
+def function(lista, liczba):
+  return liczba in lista
+
+
+lista_testowa = [1,58,35, 7,9,45,655,3]
+liczba_testowa = 3
+
+wynik = function(lista_testowa, liczba_testowa)
+
+print(wynik)
