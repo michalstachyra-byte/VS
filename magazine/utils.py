68069d413ec507334b80992866080ceade0bf0cd
Michał Stachyra <michal.stachyra@edu.uekat.pl>
2026-01-29 22:32:00 +0100

    Usunięcie plików

--- magazine/utils.py
+++
@@ -1,2 +0,0 @@
-def format_price(price):
-    return f"{price:.2f} zł"
--- main.py
+++
@@ -1,5 +0,0 @@
-from magazine.Product import Produkt
-
-
-produkt = Produkt("Laptop", 4000)
-print(produkt.cena_z_podatkiem())
